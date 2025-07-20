abec707f3957aacc32d0ecabe7f85ef88140c832
Oluwatobi Solarin <solatobi001@gmail.com>
2025-07-20 09:18:03 -0500

    upload work

--- output.tf
+++ output.tf
@@ -1,18 +1,18 @@
-output "ec2_public_ip" {
-  value = module.ec2.public_ip
-}
+# output "ec2_public_ip" {
+#   value = module.ec2.public_ip
+# }
 
-output "s3_bucket_name" {
-  description = "The name of the S3 static bucket"
-  value       = module.s3_static.bucket_name
-}
+# output "s3_bucket_name" {
+#   description = "The name of the S3 static bucket"
+#   value       = module.s3_static.bucket_name
+# }
 
-output "s3_bucket_arn" {
-  description = "The ARN of the S3 static bucket"
-  value       = module.s3_static.bucket_arn
-}
+# output "s3_bucket_arn" {
+#   description = "The ARN of the S3 static bucket"
+#   value       = module.s3_static.bucket_arn
+# }
 
-output "cloudfront_distribution_domain" {
-  description = "CloudFront distribution domain name"
-  value       = module.cloudfront.cloudfront_domain_name
-}
+# output "cloudfront_distribution_domain" {
+#   description = "CloudFront distribution domain name"
+#   value       = module.cloudfront.cloudfront_domain_name
+# }
